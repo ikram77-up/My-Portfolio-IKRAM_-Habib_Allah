55a25996333ce30ea079fa9ea349110b22a83f7e
ikram-77 <ikramhabiballah77@gmail.com>
2026-02-16 16:43:59 +0000

    Fix: Comment out Google Analytics ID check to prevent build errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -93,10 +93,10 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
-  const GA_ID = process.env.NEXT_PUBLIC_GOOGLE_MEASUREMENT_ID
-  if (!GA_ID) {
-    throw new Error("Missing Google Analytics ID")
-  }
+  // const GA_ID = process.env.NEXT_PUBLIC_GOOGLE_MEASUREMENT_ID
+  // if (!GA_ID) {
+  //   throw new Error("Missing Google Analytics ID")
+  // }
 
   return (
     <html lang="en" suppressHydrationWarning>
@@ -110,7 +110,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
       >
         <LayoutWrapper>{children}</LayoutWrapper>
       </body>
-      <GoogleAnalytics gaId={GA_ID} />
+      {/* <GoogleAnalytics gaId={GA_ID} /> */}
     </html>
   )
 }
